6c9fda30b9e525d0384c03fa64acd81b89138874
Axel Jimenez <axely7@hotmail.com>
2021-09-28 14:09:52 -0500

    Creating sidebar and adding navlink

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route} from 'react-router';
+
+import Ordenes from './components/paginas/Ordenes'
+import Menu from './components/paginas/Menu';
+import NuevoPlatillo from './components/paginas/NuevoPlatillo';
+import Sidebar from './components/ui/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="md:flex min-h-screen">
+    <Sidebar />
+
+    <div className="md:w-3/5 xl:w-4/5 p-6">  
+      <Routes>
+        <Route path="/" element={<Ordenes></Ordenes>}></Route>
+        <Route path="/menu" element={<Menu></Menu>}></Route>
+        <Route path="/nuevo-platillo" element={<NuevoPlatillo></NuevoPlatillo>}></Route>
+      </Routes>
+    </div>
+
     </div>
   );
 }
---
+++ src/components/paginas/NuevoPlatillo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NuevoPlatillo = () => {
+    return ( 
+        <>
+        </>
+     );
+}
+ 
+export default NuevoPlatillo;
