1fb5617c5b095d8d7d4bf3e2af8cd7a4e0216e2e
Axel Jimenez <axely7@hotmail.com>
2021-09-29 22:32:45 -0500

    Applying firebase and usecontext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import {Routes, Route} from 'react-router';
+import firebase, {FirebaseContext} from './firebase';
 
 import Ordenes from './components/paginas/Ordenes'
 import Menu from './components/paginas/Menu';
@@ -7,18 +8,23 @@ import Sidebar from './components/ui/Sidebar';
 
 function App() {
   return (
-    <div className="md:flex min-h-screen">
-    <Sidebar />
+    <FirebaseContext.Provider
+      value={{
+        firebase
+      }}
+    >
+      <div className="md:flex min-h-screen">
+        <Sidebar />
 
-    <div className="md:w-3/5 xl:w-4/5 p-6">  
-      <Routes>
-        <Route path="/" element={<Ordenes></Ordenes>}></Route>
-        <Route path="/menu" element={<Menu></Menu>}></Route>
-        <Route path="/nuevo-platillo" element={<NuevoPlatillo></NuevoPlatillo>}></Route>
-      </Routes>
+        <div className="md:w-3/5 xl:w-4/5 p-6">  
+          <Routes>
+            <Route path="/" element={<Ordenes></Ordenes>}></Route>
+            <Route path="/menu" element={<Menu></Menu>}></Route>
+            <Route path="/nuevo-platillo" element={<NuevoPlatillo></NuevoPlatillo>}></Route>
+          </Routes>
+        </div>
     </div>
-
-    </div>
+    </FirebaseContext.Provider>
   );
 }
 
--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -9,8 +9,8 @@ const Sidebar = () => {
                 <p className="mt-3 text-gray-500">Administra tu restaurant en las siguientes opciones: </p>
 
                 <nav className="mt-10">
-                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" end to="/">Ordenes</NavLink>
-                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" end to="/menu">Menú</NavLink>
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" end to="/">Ordenes</NavLink>
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" end to="/menu">Menú</NavLink>
                 </nav>
             </div>
         </div>
