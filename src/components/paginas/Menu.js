630e5087ddc1696b98bbda5d0a1f15055908cbaf
Axel Jimenez <axely7@hotmail.com>
2021-10-02 16:58:36 -0500

    progress bar images uploading

--- src/components/paginas/Menu.js
+++ src/components/paginas/Menu.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import {Link} from 'react-router-dom';
+import {FirebaseContext} from '../../firebase';
 
 const Menu = () => {
     return ( 
--- src/components/paginas/NuevoPlatillo.js
+++ src/components/paginas/NuevoPlatillo.js
@@ -180,6 +180,20 @@ const NuevoPlatillo = () => {
                             ></FileUploader>
                         </div>
 
+                        { subiendo && (
+                            <div className="h-12 relative w-full border">
+                                <div className="bg-green-500 absolute left-0 top-0 text-white px-2 text-sm h-12 flex items-center" style={{ width: `${progreso}%`}}>
+                                    {progreso} %
+                                </div>
+                            </div>
+                        )}
+
+                        { urlimagen && (
+                            <p className="bg-green-500 text-white p-3 text-center my-5">
+                                Subida Exitosa
+                            </p>
+                        )}
+
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="descripcion">Descripción</label>
                             <textarea 
